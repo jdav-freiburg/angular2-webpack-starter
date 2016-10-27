f4ce5be5ce4285a51422512ed95f1c047e50c391
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2016-10-27 03:10:50 +0200

    cleanup 1

--- src/app/no-content/no-content.component.ts
+++ src/app/no-content/no-content.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'no-content',
-  template: `
-    <div>
-      <h1>404: page missing</h1>
-    </div>
-  `
+    selector: 'no-content',
+    template: '<span>FIXME - 404: page missing</span>'
 })
 export class NoContentComponent {
 

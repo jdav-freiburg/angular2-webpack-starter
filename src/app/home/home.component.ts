b664b824a8e3561520d30145b880ae188a222358
Johannes Gest <Johannes.Gest@haufe-lexware.com>
2016-10-30 01:17:28 +0200

    example database access

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     selector: 'home',
     styleUrls: ['./home.component.css'],
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+    private items: FirebaseListObservable<any[]>;
+
+    constructor(private angularFire: AngularFire) {
+    }
+
+    ngOnInit(): void {
+        this.items = this.angularFire.database.list('/items');
+    }
+
 }
